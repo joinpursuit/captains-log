bb2a0357762d7bc636757bd4848911aaacfc96c5
cassidybeni <cassidybeni@pursuit.org>
2021-06-08 15:31:28 -0400

    Create logs.js

---
+++ models/logs.js
@@ -0,0 +1,25 @@
+module.exports = [
+  {
+    captainName: "Picard",
+    title: "Stars",
+    post: "Today I contemplated that there sure are a lot of stars in the sky",
+    mistakesWereMadeToday: true,
+    daysSinceLastCrisis: 100,
+  },
+
+  {
+    captainName: "Jack Sparrow",
+    title: "Ship",
+    post: "Today the ship had a leak",
+    mistakesWereMadeToday: false,
+    daysSinceLastCrisis: 101,
+  },
+
+  {
+    captainName: "Hook",
+    title: "Pirates",
+    post: "Today we were almost robbed by pirates",
+    mistakesWereMadeToday: true,
+    daysSinceLastCrisis: 1,
+  },
+];
