dc120f0727c9df25764ac4c229702fa3f81ac849
Hady Mohamed <hadymohamed@pursuit.org>
2021-06-08 17:01:56 -0400

    set up app.js, server.js, log.js, and logsController.js

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const logsController = require("./controllers/logsController.js");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the captain's log!");
+});
+
+app.use("/logs", logsController);
+
+app.get("*", (req, res) => {
+  res.send(
+    `<h1>404 Page Not Found!</h1> <img src="https://i.ebayimg.com/00/s/NDk5WDc1MA==/z/TrAAAOSwFL9TsF6Z/$_3.JPG"/>`,
+  );
+});
+
+module.exports = app;
---
+++ models/log.js
@@ -0,0 +1,23 @@
+module.exports = [
+  {
+    captainName: "Picard",
+    title: "Stars",
+    post: "Today I contemplated that there sure are a lot of stars in the sky",
+    mistakesWereMadeToday: true,
+    daysSinceLastCrisis: 100,
+  },
+  {
+    captainName: "Worf",
+    title: "Starfleet",
+    post: "I am Worf. Son of Mogh.",
+    mistakesWereMadeToday: false,
+    daysSinceLastCrisis: 40,
+  },
+  {
+    captainName: "Spock",
+    title: "Enterprise",
+    post: "Live long and prosper.",
+    mistakesWereMadeToday: true,
+    daysSinceLastCrisis: 200,
+  },
+];
