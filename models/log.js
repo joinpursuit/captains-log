bfbb02dad4c392b96fdeea91faca3a7323788bac
Daniel Naranjo <MR.DANIEL.NARANJO@PROTONMAIL.COM>
2021-06-08 16:47:38 -0400

    Logs added

---
+++ models/log.js
@@ -0,0 +1,38 @@
+module.exports = [
+    {
+      captainName: "Picard",
+      title: "Stars",
+      post: "Today I contemplated that there sure are a lot of stars in the sky",
+      mistakesWereMadeToday: true,
+      daysSinceLastCrisis: 100,
+    },
+    {
+      captainName: "Picard",
+      title: "Dinner",
+      post: "In the nigth I ate frozen chicen again",
+      mistakesWereMadeToday: false,
+      daysSinceLastCrisis: 20,
+    },
+    {
+      captainName: "Picard",
+      title: "Planet",
+      post: "Today we reach another planet in our trip to Andromeda",
+      mistakesWereMadeToday: true,
+      daysSinceLastCrisis: 6,
+    },
+    {
+      captainName: "Picard",
+      title: "Civilization",
+      post: "Long Afternoon today,because  I met another civilization",
+      mistakesWereMadeToday: false,
+      daysSinceLastCrisis: 10,
+    },
+    {
+      captainName: "Picard",
+      title: "Fuel Corrupted",
+      post: "Today I found galaxy roaches in the fuel tank",
+      mistakesWereMadeToday: true,
+      daysSinceLastCrisis: 2,
+    }
+    ]
+    
