c22eb289292dd74ceb014669548fdb2615fa1b17
Yashiry Azize <azizeyashiry@gmail.com>
2021-06-08 20:19:07 -0400

    set up complete

---
+++ server.js
@@ -0,0 +1,12 @@
+const { response } = require("express");
+const app = require("./app");
+
+
+require("dotenv").config();
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+    console.log("Listening on PORT", PORT)
+})
+
