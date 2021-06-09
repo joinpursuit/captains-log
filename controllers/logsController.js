151b904c525730e185479021c1caff81c9d89544
Durdona Djalilova <ddjalilova3@gmail.com>
2021-06-08 20:55:05 -0400

    part 1 done

---
+++ controllers/logsController.js
@@ -0,0 +1,8 @@
+const logs = require ("express").Router()
+const logsArray = require("../models/log")
+
+logs.get("/", (req, res)=>{
+    res.json(logsArray)
+})
+
+module.exports = logs
---
+++ models/log.js
@@ -0,0 +1,23 @@
+module.exports = [
+  {
+    captainName: "Picard",
+    title: "Stars",
+    post: "Today I contemplated that there sure are a lot of stars in the sky",
+    mistakesWereMadeToday: true,
+    daysSinceLastCrisis: 100,
+  },
+  {
+    captainName: "Corey",
+    title: "Lil Mama",
+    post: "Today I taught class how to route their way into sea",
+    mistakesWereMadeToday: true,
+    daysSinceLastCrisis: 1,
+  },
+  {
+    captainName: "Mashu",
+    title: "Soccer player",
+    post: "Today I introduced myself to the team",
+    mistakesWereMadeToday: false,
+    daysSinceLastCrisis: 0,
+  },
+];
