b494e9f70dc9ab8af63f3e4754b2154dfb8e0387
anasnahil <nahil.anas1@gmail.com>
2021-06-08 21:19:10 -0400

    lab done with carlo

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const logsController = require('./controllers/logsController')
+app.get('/', (req, res) => {
+    res.send(`welcome to the captain's log`)
+})
+
+app.use('/logs', logsController);
+
+app.get("*", (req, res) => {
+    res.status(404).send("Page not found");
+});
+
+module.exports = app;
