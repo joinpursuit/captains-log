33fc64ed490908a48cbb090a0b424ea3ab95092e
Hady Mohamed <hadymohamed@pursuit.org>
2021-06-09 14:07:37 -0400

    created :id path

--- controllers/logsController.js
+++ controllers/logsController.js
@@ -6,6 +6,14 @@ logs.get("/", (req, res) => {
   res.json(logsArray);
 });
 
+logs.get("/:id", (req, res) => {
+  if (logsArray[req.params.id]) {
+    res.json(logsArray[req.params.id]);
+  } else {
+    res.redirect("/404");
+  }
+});
+
 logs.post("/", (req, res) => {
   logsArray.push(req.body);
   res.json(logsArray[logsArray.length - 1]);
