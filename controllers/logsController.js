189a48cdf84ba9d779f443f3defcc62b4c109eb1
Shoaib Dar <SDAR30@gmail.com>
2021-06-08 21:59:54 -0400

    added alphabetical order

--- controllers/logsController.js
+++ controllers/logsController.js
@@ -3,6 +3,21 @@ const logs = express.Router();
 const logArray = require("../models/log")
 
 logs.get("/", (req ,res)=>{
+    const {order} = req.query
+    console.log(req.query) // {order}
+    if(order === "asc"){
+        logArray.sort((a, b)=>{
+            if(a.captainName < b.captainName) { return -1; }
+            if(a.captainName > b.captainName) { return 1; }
+            return 0;
+        })
+    }else if (order === "desc"){
+        logArray.sort((b, a)=>{
+            if(a.captainName < b.captainName) { return -1; }
+            if(a.captainName > b.captainName) { return 1; }
+            return 0;
+        })
+    }
     res.json(logArray)
 })
 
