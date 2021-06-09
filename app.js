e709b3c0be1b91d7a7a85d306c5717dab29defaf
Teyanna Earle <teyonna.earle92@gmail.com>
2021-06-09 13:02:59 -0400

    Complete part 2 and 3

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const app = express()
+const logsController = require("./controllers/logsController")
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("Welcome")
+    res.send("Welcome to the captain's log")
 })
+
+app.use("/logs", logsController)
+
+app.get("*", (req, res) => {
+    res.status(404).send("Page not found");
+});
 
 module.exports = app
