30160be3ef45a6ac64a920186142f9dcbf1e1bf8
Talia Pichardo <taliapichardo@gmail.com>
2021-06-10 16:48:07 -0400

    passing all tests

--- app.js
+++ app.js
@@ -5,10 +5,15 @@ const app = express()
 
 app.use(express.json())
 app.use('/logs', logsController)
+// app.use((req, res, next) => {
+//     console.log("the middleware is runnin g")
+//     next()
+// })
 
 app.get("/", (req, res) => {
     res.send('welcome to the captain\'s log')
 })
+
 
 app.get("*", (req, res) => {
     res.status(404).send('RRRRRRRR, Page not found')
