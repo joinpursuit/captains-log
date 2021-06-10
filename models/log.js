ccb23909e3a73fd5483608314373812a2b51af04
Jenna Qian <jiayin1996@gmail.com>
2021-06-10 15:38:51 -0400

    Complete Lab

---
+++ models/log.js
@@ -0,0 +1,24 @@
+module.exports = [
+    {
+      captainName: "Picard",
+      title: "Stars",
+      post: "Today I contemplated that there sure are a lot of stars in the sky",
+      mistakesWereMadeToday: true,
+      daysSinceLastCrisis: 100,
+    },
+    {
+        captainName: "John",
+        title: "Stars",
+        post: "Today I contemplated that there sure are a lot of stars in the sky",
+        mistakesWereMadeToday: false,
+        daysSinceLastCrisis: 80,
+    },
+    {
+        captainName: "Mary",
+        title: "Stars",
+        post: "Today I contemplated that there sure are a lot of stars in the sky",
+        mistakesWereMadeToday: true,
+        daysSinceLastCrisis: 500,
+    }
+]
+    
