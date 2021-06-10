7c69d6ae2cc21e03c23ec1e8eaa45175102c5e91
Daniel Naranjo <MR.DANIEL.NARANJO@PROTONMAIL.COM>
2021-06-10 15:07:45 -0400

    Middleware added

--- app.js
+++ app.js
@@ -9,23 +9,16 @@ const logsArray = require('./models/log.js')
 
 // Routes
 app.use((req,res, next) =>{ 
-    console.log("we are in the midleware")
     next()
  } )
 
- app.use(express.json())
+app.use(express.json())
 
 app.get("/", (req, res) => { 
     res.send("Welcome to the captain's log")
  })
 
 app.use("/logs", logs )
-
-app.get("/logs/:index", (req, res) =>{ 
-    const { index } = req.params
-
-    res.send(logsArray[index])
- })
 
 
 app.get("*", (req, res) => { 
