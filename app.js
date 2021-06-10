6139a9996d2e28e61978bcfdb3e25cbd46e3994e
Talia Pichardo <taliapichardo@gmail.com>
2021-06-09 20:00:17 -0400

    second part

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
+const logsController = require('./controllers/logsController.js');
+
 const app = express()
+
+app.use(express.json())
+app.use('/logs', logsController)
 
 app.get("/", (req, res) => {
     res.send('welcome to the captain\'s log')
 })
-
-const index = require('./controllers/logsController.js');
-app.use('/logs', index)
 
 app.get("*", (req, res) => {
     res.status(404).send('RRRRRRRR, Page not found')
--- controllers/logsController.js
+++ controllers/logsController.js
@@ -6,4 +6,17 @@ logs.get("/", (req, res) => {
     res.json(captArr)
 })
 
+logs.get("/:id", (req, res) => {
+    const { id } = req.params
+    if (captArr[id]) {
+        res.json(captArr[id])
+    } else {
+        res.redirect('/404')
+    }
+})
+
+logs.post("/", (req, res) => {
+    captArr.push(req.body);
+    res.json(captArr[captArr.length - 1]);
+})
 module.exports = logs;
