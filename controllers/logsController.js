3674116f75530f9ad401610c3896e1ecb3261615
EsayH <ez@EsayHs-Mac-mini.local>
2021-06-10 17:44:29 -0400

    added url validation

--- controllers/logsController.js
+++ controllers/logsController.js
@@ -2,6 +2,24 @@ const express = require('express')
 const logs = express.Router()
 const logsArray = require('../models/log')
 
+const validateURL = (req, res, next) => {
+  const http = "http://"
+  const https = "https://"
+  var validURL = req.protocol + '://' + req.get('host') + req.url
+  console.log(`[development] REQUEST URL: ${validURL}`)
+
+  if (
+    validURL.substring(0, 7) === http ||
+    validURL.substring(0, 8) === https
+  ) {
+    return next()
+  } else {
+    res.status('400').send("Sorry, Invalid URL")
+  }
+}
+
+logs.use(validateURL)
+
 logs.get('/', (req, res) => {
 	res.status(200).json(logsArray)
 })
