2044162d46126abe7056561bbe5544255ba4edf4
Marsha Hall <mphall77@gmail.com>
2021-06-10 09:49:27 -0400

    fix the test file, pass CREATE and POST

--- app.js
+++ app.js
@@ -1,93 +1,33 @@
 // DEPENDENCIES
 const express = require("express");
+// import logsArray
+const logsArray = require("./models/log");
 
 // CONFIGURATION
 const app = express();
-// import logsArray
-const logsArray = require("./models/log");
+
 // connect the controller for logs
 const logsController = require("./controllers/logsController");
+
+// MIDDLEWARE
+app.use(express.json());
+
+app.use((req, res, next) => {
+	console.log(`testing 101`);
+	next();
+});
 
 // ROUTES
 //  ** use logsController on any path that starts with logs
 app.use("/logs", logsController);
 
+// ROOT
 app.get("/", (req, res) => {
+	res.send("Welcome to my Captain's Log App");
+});
+
+app.get("/logs", (req, res) => {
 	res.json(logsArray);
-});
-
-// /logs?order=asc or desc
-//localhost:3004/logs/search?order=desc
-http: app.get("/logs/search?", (req, res) => {
-	if (order === "asc") {
-		res.json(
-			logsArray
-				.map((entry) => {
-					return entry.post;
-				})
-				.sort()
-		);
-	} else if (order === "desc") {
-		res.json(
-			logsArray
-				.map((entry) => {
-					return entry.post;
-				})
-				.sort()
-				.reverse()
-		);
-	}
-});
-
-//logs?mistakes=true or /logs?mistakes=false
-app.get("/logs/mistakes?", (req, res) => {
-	const { mistakes } = req.query;
-
-	// mistakes ? mistakes.mistakesWereMadeToday : !mistakes.mistakesWereMadeToday;
-
-	res.json(
-		logsArray.map((mistakes) => {
-			if (mistakes.mistakesWereMadeToday) {
-				return mistakes.mistakesWereMadeToday.filter(true);
-			} else {
-				return "Nope";
-			}
-		})
-	);
-	if (mistakes === true) {
-		res.json(
-			logsArray.map((mistakes) => {
-				return mistakes.mistakesWereMadeToday;
-			})
-		);
-	} else {
-		return res.json(
-			logsArray.map((mistakes) => {
-				return "no mistakes";
-			})
-		);
-	}
-});
-
-// order the captains
-// localhost:3004/logs/captain?order=asc
-app.get("/logs/captain?", (req, res) => {
-	const { order } = req.query;
-	if (order === "asc") {
-		res.json(
-			logsArray
-				.map((captain) => {
-					return captain.captainName;
-				})
-				.sort()
-		);
-	} else {
-		res.json(
-			logsArray.map((captain) => {
-				return captain.captainName;
-			})
-		);
-	}
 });
 
 // 404 PAGE
@@ -97,3 +37,24 @@ http: app.get("*", (req, res) => {
 
 // EXPORT
 module.exports = app;
+
+// order the captains
+// localhost:3004/logs/captain?order=asc
+// app.get("/logs", (req, res) => {
+// 	const { order } = req.query;
+// 	if (order === "asc") {
+// 		res.json(
+// 			logsArray
+// 				.map((captain) => {
+// 					return captain.captainName;
+// 				})
+// 				.sort()
+// 		);
+// 	} else {
+// 		res.json(
+// 			logsArray.map((captain) => {
+// 				return captain.captainName;
+// 			})
+// 		);
+// 	}
+// });
--- controllers/logsController.js
+++ controllers/logsController.js
@@ -9,5 +9,45 @@ logs.get("/", (req, res) => {
 	res.json(logsArray);
 });
 
-// export logs
+// CREATE
+logs.get("/:id", (req, res) => {
+	const { id } = req.params;
+	if (logsArray[id]) {
+		res.json(logsArray[id]);
+	} else {
+		res.redirect("/404");
+	}
+});
+
+// POST
+logs.post("/", (req, res) => {
+	logsArray.push(req.body);
+	res.json(logsArray);
+});
+
+// EXPORT
 module.exports = logs;
+
+// BONUS
+// logs.get("/", (req, res) => {
+// 	const { order } = req.query;
+// 	// take in the query string for ascending or descending
+// 	if (order === "asc") {
+// 		res.json(
+// 			logsArray
+// 				.map((entry) => {
+// 					return entry.post;
+// 				})
+// 				.sort()
+// 		);
+// 	} else if (order === "desc") {
+// 		res.json(
+// 			logsArray
+// 				.map((entry) => {
+// 					return entry.post;
+// 				})
+// 				.sort()
+// 				.reverse()
+// 		);
+// 	}
+// });
