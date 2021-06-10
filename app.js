7b4c6dfa56c367815189a7b3a81d0d01b91d96f7
anasnahil <nahil.anas1@gmail.com>
2021-06-09 21:36:38 -0400

    lab done with adam

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ app.get('/', (req, res) => {
     res.send(`welcome to the captain's log`)
 })
 
+app.use(express.json());
+
+app.use((req, res, next) => {
+    console.log("This code runs for every request");
+    next();
+});
+  
 app.use('/logs', logsController);
 
 app.get("*", (req, res) => {
--- controllers/logsController.js
+++ controllers/logsController.js
@@ -6,5 +6,8 @@ logs.get('/', (req, res) => {
     res.json(logsArray)
 })
 
-
+logs.post('/', (req, res) => {
+    logsArray.push(req.body);
+    res.json(logsArray[logsArray.length - 1])
+})
 module.exports = logs
