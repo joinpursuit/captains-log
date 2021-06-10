4d3f1f35ea84248d2fd197f0f3b4daa53fe1930c
EsayH <ez@EsayHs-Mac-mini.local>
2021-06-10 17:29:10 -0400

    added conditionals

--- controllers/logsController.js
+++ controllers/logsController.js
@@ -3,35 +3,41 @@ const logs = express.Router()
 const logsArray = require('../models/log')
 
 logs.get('/', (req, res) => {
-  res.status(200).json(logsArray)
+	res.status(200).json(logsArray)
 })
 
 logs.get('/:id', (req, res) => {
-  const { id } = req.params
-  if (logsArray[id]) {
-    res.json(logsArray[id])
-  } else {
-    res.redirect('/404')
-  }
+	const { id } = req.params
+	if (logsArray[id]) {
+		res.json(logsArray[id])
+	} else {
+		res.redirect('/404')
+	}
 })
 
 logs.post('/', (req, res) => {
-  logsArray.push(req.body)
-  res.json(logsArray[-1])
-})
-
-logs.delete('/:id', (req, res) => {
-  const { id } = req.params
-  const deletedLogs = logsArray.splice(id, 1)
-  res.status(200).json(deletedLogs)
+	logsArray.push(req.body)
+	res.json(logsArray[-1])
 })
 
 logs.put('/:id', (req, res) => {
-  const { id } = req.params
-  logsArray[id] = req.body
-  res.status(200).json(logsArray[id])
+	const { id } = req.params
+	if (logsArray[id]) {
+		logsArray[id] = req.body
+		res.status(200).json(logsArray[id])
+	} else {
+		res.redirect('/404')
+	}
 })
 
+logs.delete('/:id', (req, res) => {
+	const { id } = req.params
+	if (logsArray[id]) {
+		const deletedLogs = logsArray.splice(id, 1)
+		res.status(200).json(deletedLogs)
+	} else {
+		res.redirect('/404')
+	}
+})
 
 module.exports = logs
-
