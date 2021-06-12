8118b86777864beb4c69690b18e03cb7291514de
joshuamendez <joshuafmendez@gmail.com>
2021-06-12 16:49:10 -0400

    added cors to app file

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require("express");
+const cors = require('cors');
 const app = express();
-
 const logsController = require("./controllers/logsController.js");
 const v2LogsController = require("./v2/controllers/logsController");
 
 app.use(express.json());
+app.use(cors());
 app.use((req, res, next) => {
   next();
 });
