84e850aae03761e25d9e0e0c087bec77b7aad489
Coreen Cooper <coreencooper@pursuit.org>
2021-06-12 11:59:36 -0400

    add PUT request

--- controllers/logsController.js
+++ controllers/logsController.js
@@ -2,24 +2,38 @@ const logs = require("express").Router();
 const logsArray = require("../models/logs");
 
 logs.get("/:id", (req, res) => {
-    const { id } = req.params;
-    if(logsArray[id]) {
-        console.log(logsArray[id])
-        res.json(logsArray[id])
-    } else {
-    res.redirect("/*")
-    }
-})
+  const { id } = req.params;
+  if (logsArray[id]) {
+    console.log(logsArray[id]);
+    res.json(logsArray[id]);
+  } else {
+    res.redirect("/*");
+  }
+});
 
+// part 2
 logs.get("/", (req, res) => {
-    res.json(logsArray);
+  res.json(logsArray);
 });
 
 logs.post("/", (req, res) => {
-    logsArray.push(req.body);
-    console.log
-    res.json(logsArray[logsArray.length - 1])
-})
+  logsArray.push(req.body);
+  console.log;
+  res.json(logsArray[logsArray.length - 1]);
+});
 
+// part 3
 
-module.exports = logs
+logs.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const { body } = req;
+  if (logsArray[id]) {
+      logsArray[id] = body
+      res.json(logsArray[id])
+      console.log(body)
+} else {
+      res.redirect("/*");
+  }
+});
+
+module.exports = logs;
