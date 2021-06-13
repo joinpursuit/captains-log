ab6392f9f6f6b156b5029326b681001ad4108616
Shoaib Dar <SDAR30@gmail.com>
2021-06-13 10:18:39 -0400

    updated server" "

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 app.use(express.json()); // Parse incoming JSON // this adds "body" to req object
+app.use(cors())
 
 //MIDDLEWARE
 app.use((req, res, next)=>{
