5b8aa52c862c1a853d26efb8226b451ad513a596
anjuhypp <anjuhypp@gmail.com>
2021-06-14 19:35:55 -0400

    complete part 3

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const app = express()
 
+app.use(express.json())
+
 const logsController = require("./controllers/logsController.js");
 
 app.get("/", (req, res) => {
--- controllers/logsController.js
+++ controllers/logsController.js
@@ -1,20 +1,48 @@
 const logs = require("express").Router();
+// const bookmarks = require("../../bookmarks/controllers/bookmarksController.js");
 const logsArray = require("../models/log.js");
+
+logs.get("/:arrayIdx", (req, res) => {
+  const log = logsArray[req.params.arrayIdx];
+  if (log) {
+    res.json(log);
+  } else {
+    res.redirect("/404");
+  }
+});
 
 logs.get("/", (req, res) => {
   res.json(logsArray);
 });
 
+logs.put("/:arrayIdx", (req, res) => {
+  const { arrayIdx } = req.params;
+  console.log(arrayIdx)
+  const { body } = req;
+  console.log(body)
+  logsArray[arrayIdx] = body;
+  res.json(logsArray[arrayIdx]);
+});
 
-logs.get("/:arrayIdx", (req, res) => {
-  const log = logsArray[req.params.arrayIdx]
-  if(log) {
-    res.json(log);
-  } else {
-    res.redirect('/404');
-  }gi 
+logs.post("/", (req, res) => {
+  const { body } = req;
+  logsArray.push(body);
+  const newIdx = logsArray.length - 1;
+  res.json(logsArray[newIdx]);
+});
 
-})
+logs.delete("/:arrayIdx", (req, res) => {
+  // const { body } = req;
+  const deletedlog = logsArray.splice(req.params.indexArray, 1);
+  res.status(200).json(deletedlog);
+  // const newIdx = logsArray.length - 1;
+  // res.json(logsArray[newIdx]);
+});
 
+// DELETE
+// bookmarks.delete("/:indexArray", (req, res) => {
+//   const deletedBookmark = bookmarkArray.splice(req.params.indexArray, 1);
+//   res.status(200).json(deletedBookmark);
+// });
 
 module.exports = logs;
