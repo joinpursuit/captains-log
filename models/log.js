52e1ca4cc58060cbccec4eaa6a963ac28115c50d
Jonathan Bedon <bedonjonathan@gmail.com>
2021-06-14 21:02:41 -0400

    added more data

--- models/log.js
+++ models/log.js
@@ -1,28 +1,4 @@
 logsArray = [
-<<<<<<< HEAD
-    {
-      captainName: "Picard",
-      title: "Stars",
-      post: "Today I contemplated that there sure are a lot of stars in the sky",
-      mistakesWereMadeToday: true,
-      daysSinceLastCrisis: 30,
-    },
-    {
-      captainName: "Kenia",
-      title: "Star Lord Queen",
-      post: "Today I ate chipotle in the stars",
-      mistakesWereMadeToday: true,
-      daysSinceLastCrisis: 15,
-    },
-    {
-      captainName: "Jonathan",
-      title: "Uranus King",
-      post: "Today I configured a circuits board",
-      mistakesWereMadeToday: true,
-      daysSinceLastCrisis: 1,
-    }
-    ]
-=======
   {
     captainName: "Picard",
     title: "Courage",
@@ -87,6 +63,5 @@ logsArray = [
     daysSinceLastCrisis: 100,
   },
 ];
->>>>>>> 1ce85495c633eae016b9d1d4d123eb2aea5a170e
 
 module.exports = logsArray;
