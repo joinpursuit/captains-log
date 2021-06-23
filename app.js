12305f3ff2d4ebdfcbf8cfa339db89b88ffdc02f
Grace Bong <gracebong.park@gmail.com>
2021-06-23 00:50:34 -0400

    part 1 with bonus complete

--- app.js
+++ app.js
@@ -3,14 +3,16 @@ const express = require('express');
 
 //Configuration
 const app = express();
-const logs = require('./controllers/logsController');
+const logsController = require('./controllers/logsController');
+
+app.use(express.json());
 
 //Routes
 app.get('/', (req, res) => {
     res.send(`Welcome to the captain's log`)
 });
 
-app.use('/logs', logs)
+app.use('/logs', logsController)
 
 app.get('*', (req, res) => {
     res.status(404).send('Page not found.')
--- controllers/logsController.js
+++ controllers/logsController.js
@@ -1,15 +1,45 @@
 const express = require('express');
 const logs = express.Router();
-const captainLogs = require('../logs/captainLogs.js');
+const logsArray = require('../models/logs.js');
 
 logs.get('/', (req, res) => {
     const query = req.query;
+    
+    if(query.order === 'asc') {
+        let sorted = logsArray.sort((a, b) => 
+            a.title > b.title ? 1 : -1
+        );
+        res.status(200).json(sorted);
+    } else if (query.order === 'desc') {
+        let sorted = logsArray.sort((a, b) => 
+            a.title < b.title ? 1 : -1
+        );
+        res.status(200).json(sorted);
+    };
 
-    if (query.order === 'asc') {
-        
-    }
+    if(query.mistakes === 'true') {
+        let filtered = logsArray.filter(log => log.mistakesWereMadeToday === true);
+        res.status(200).json(filtered);
+    } else if (query.mistakes === 'false') {
+        let filtered = logsArray.filter(log => log.mistakesWereMadeToday === false);
+        res.status(200).json(filtered);
+    };
+    
+    if(query.lastCrisis === 'gt10') {
+        let filtered = logsArray.filter(log => log.daysSinceLastCrisis > 10);
+        res.status(200).json(filtered);
+    } else if (query.lastCrisis === 'gte20') {
+        let filtered = logsArray.filter(log => log.daysSinceLastCrisis >= 20);
+        res.status(200).json(filtered);
+    } else if (query.lastCrisis === 'lte5') {
+        let filtered = logsArray.filter(log => log.daysSinceLastCrisis <= 5);
+        res.status(200).json(filtered);
+    };
 
-    res.status(200).json(captainLogs)
+    res.status(200).json(logsArray);
 });
 
-module.exports = logs
+
+
+
+module.exports = logs;
