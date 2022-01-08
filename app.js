22db2a339be1e4c5e7d332b2840ddc8c6c6f60fb
Lakisha J <lakishajohnson@pursuit.org>
2022-01-08 08:11:25 -0500

    Initial commit

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const express = require("express");
 const logController = require("./controllers/captainsLogs")
 const app = express
 
-app.request("/captainsLogs", logController);
+app.use("/captainsLogs", logController);
 
-app.length("/", (req, res) => {
+app.get("/", (req, res) => {
     console.log("Get request to /")
     res.send("Welcome to the captain's log")
 })
+
+
+module.exports = app;
