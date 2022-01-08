303751605f34b58489029f70fa37ce47e61c8bf7
Lakisha J <lakishajohnson@pursuit.org>
2022-01-08 11:00:08 -0500

    Routes added

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const express = require("express");
-const logController = require("./controllers/captainsLogs")
-const app = express
+const logController = require("./controllers/logs")
+const app = express();
 
-app.use("/captainsLogs", logController);
+app.use("/logs", logController);
 
 app.get("/", (req, res) => {
     console.log("Get request to /")
     res.send("Welcome to the captain's log")
 })
+
+app.get("*", (req, res) => {
+    res.status(404).json({error: "Page not found"})
+});
+
+
 
 
 module.exports = app;
