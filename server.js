bbd4aafd07fb3a08dafb1b939115d60594376224
Lakisha J <lakishajohnson@pursuit.org>
2022-01-08 11:00:43 -0500

    PORT listening

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
-const app = require("./app")
+const app = require("./app.js");
 
-const dotenv = require("dotenv")
-dotenv.config()
 
-const PORT = process.env.PORT
+const PORT = 3003
 
 app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`)
-})
+  console.log(`Listening on port ${PORT}`);
+});
