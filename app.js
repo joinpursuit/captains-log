48bab83e5750a74205acb82b18023e327b430e04
JoseBorbon <joserborbon@gmail.com>
2022-01-07 22:13:19 -0500

    Part 3 done, 7 figures are coming

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 //Dependencies
 const express = require('express');
 const logsController = require(`${__dirname}/controllers/logsController`);
+
 //Configurations
 const app = express();
+
+// Middleware
+app.use(express.json()); // Parse incoming JSON
+
+app.use((req, res, next) => {
+  console.log('This code runs for every request');
+  next();
+});
+
+// Routes
 app.use('/logs', logsController);
 
 app.get('/', (req, res) => {
--- controllers/logsController.js
+++ controllers/logsController.js
@@ -26,4 +26,25 @@ logs.get('/', (req, res) => {
   });
 });
 
+logs.get('/invalid', (req, res) => {
+  res.status(404).json({ status: 'fail', error: 'invalid index' });
+});
+
+logs.get('/:arrayIndex', (req, res) => {
+  const idx = Number(req.params.arrayIndex);
+  if (!logsArray[idx]) res.redirect('/logs/invalid');
+  res.json(logsArray[idx]);
+});
+
+logs.post('/', (req, res) => {
+  logsArray.push(req.body);
+  res.status(201).json(logsArray[logsArray.length - 1]);
+});
+
+logs.delete('/:arrayIndex', (req, res) => {
+  const idx = Number(req.params.arrayIndex);
+  logsArray.splice(idx, 1);
+  res.status(204).json(logsArray);
+});
+
 module.exports = logs;
