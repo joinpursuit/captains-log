7d3f7e5e02ae9d196789e56ab4310a63ad5c0faa
Lakisha J <lakishajohnson@pursuit.org>
2022-01-08 23:58:34 -0500

    Working on sortingjson file

--- controllers/logs.js
+++ controllers/logs.js
@@ -1,15 +1,33 @@
 const express = require("express");
-const log = express.Router()
+const logs = express.Router()
 
 const logArray = require("../models/log");
 
-log.get("/", (req, res) => {
+logs.get("/", (req, res) => {
     console.log("Get request to /logs")
     res.send(logArray)
 });
 
-log.post("/", (req, res) => {
-    
+logs.get("/:arrayIndex", (req, res) => {
+    console.log("Get request to /arrayIndex")
+    const { arrayIndex } = req.params;
+    if(logArray[Number(arrayIndex)]) {
+        res.send(logArray[Number(arrayIndex)])
+    } else {
+        res.redirect("/*")
+    }
+});
+
+logs.get("/logs?order=asc", (req, res) => {
+    console.log("You should be in order")
+    logArray.sort((x, y) => {
+        let a = a.captainName.toUpperCase(),
+            b = b.captainName.toUpperCase();
+        return a == b ? 0 : a > b ? 1 : -1
+    })
+    res.send(logArray)
 })
 
-module.exports = log;
+
+
+module.exports = logs;
