2466e268edf80d6b7d9389046db24008d85804c5
Tashawnee Guarriello <tashawneeguarriello@pursuit.org>
2022-01-09 10:27:23 -0500

    Created Express App and Created Route for Home

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+
+app.use("/logs");
+
+app.get("/", (req, res) => {
+  console.log("GET request to /");
+  res.send("Welcome to the Captain's Log");
+});
+
+module.exports = app;
