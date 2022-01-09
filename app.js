018795a1eef532d54f222b057baeaff715b7ec6c
Rohan-Sterling <rohansterling@pursuit.org>
2022-01-09 12:19:08 -0500

    add arrayIndex route

--- app.js
+++ app.js
@@ -11,6 +11,15 @@ app.get('/logs', (request, response) => {
     response.send(logsArray)
 })
 
+app.get("/logs/:arrayIndex", ( request, response)=> {
+    const {arrayIndex: index} = request.params
+    response.send(logsArray[index])
+})
+
+// app.post("/logs", (request , response)=> {
+//     response.send("Post request to logs")
+// })
+
 app.get('*', (request, response)=>{
     response.sendStatus(404)
 })
