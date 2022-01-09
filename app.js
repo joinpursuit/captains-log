1ba2c04d09c79adc2b354d5c80eb8a85ed9b91f0
Rohan-Sterling <rohansterling@pursuit.org>
2022-01-09 12:06:28 -0500

    add log route sending logs Array

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const express = require("express")
+const logsArray = require("./models/log")
 
 const app = express()
 
 app.get('/', (request, response)=> {
     response.send("Home")
+})
+
+app.get('/logs', (request, response) => {
+    response.send(logsArray)
+})
+
+app.get('*', (request, response)=>{
+    response.sendStatus(404)
 })
 
 
