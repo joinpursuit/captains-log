968cd600001a43790ecdb4e74721e9a6cb3e958a
Lakisha J <lakishajohnson@pursuit.org>
2022-01-09 00:00:09 -0500

    404 error added

--- app.js
+++ app.js
@@ -7,7 +7,18 @@ app.use("/logs", logController);
 app.get("/", (req, res) => {
     console.log("Get request to /")
     res.send("Welcome to the captain's log")
-})
+});
+
+
+// app.get("/logs/?order=asc", (req, res) => {
+//     console.log("Yall should be in order")
+//     logArray.sort((x, y) => {
+//         let a = a.captainName.toUpperCase(),
+//             b = b.captainName.toUpperCase();
+//         return a == b ? 0 : a > b ? 1 : -1
+//     })
+//     res.send(logArray)
+// })
 
 app.get("*", (req, res) => {
     res.status(404).json({error: "Page not found"})
