6f24c1585ae6e910c968867786de1e1ace9b6abf
stephanie lucano <stephaniemarielucano@gmail.com>
2022-01-10 01:30:27 -0500

    Created a route /logs that sends the logs array

--- app.js
+++ app.js
@@ -6,4 +6,8 @@ app.get("/", (request, response) => {
     response.send("Welcome to the Captain's Log App")
 })
 
+app.get("/logs", (request, response) => {
+    response.send(logsArray)
+})
+
 module.exports = app
