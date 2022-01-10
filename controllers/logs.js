8851d5065012eeaf2d4221a2b95edfab9689847a
Lakisha J <lakishajohnson@pursuit.org>
2022-01-10 16:07:55 -0500

    Sort Ascending functional

--- controllers/logs.js
+++ controllers/logs.js
@@ -1,33 +1,34 @@
 const express = require("express");
-const logs = express.Router()
+const logs = express.Router();
 
 const logArray = require("../models/log");
 
 logs.get("/", (req, res) => {
-    console.log("Get request to /logs")
-    res.send(logArray)
+  console.log("Get request to /logs");
+  console.log(req.query);
+  const { order } = req.query;
+  if (req.originalUrl === "/logs") {
+    res.send(logArray);
+  } else {
+    const arr = [...logArray];
+    if (!!order) {
+      arr.sort((a, b) => {
+        (a = a.captainName.toUpperCase()), (b = b.captainName.toUpperCase());
+        return a === b ? 0 : a > b ? 1 : -1;
+      });
+    }
+    res.send(arr);
+  }
 });
 
 logs.get("/:arrayIndex", (req, res) => {
-    console.log("Get request to /arrayIndex")
-    const { arrayIndex } = req.params;
-    if(logArray[Number(arrayIndex)]) {
-        res.send(logArray[Number(arrayIndex)])
-    } else {
-        res.redirect("/*")
-    }
+  console.log("Get request to /arrayIndex");
+  const { arrayIndex } = req.params;
+  if (logArray[Number(arrayIndex)]) {
+    res.send(logArray[Number(arrayIndex)]);
+  } else {
+    res.redirect("/*");
+  }
 });
-
-logs.get("/logs?order=asc", (req, res) => {
-    console.log("You should be in order")
-    logArray.sort((x, y) => {
-        let a = a.captainName.toUpperCase(),
-            b = b.captainName.toUpperCase();
-        return a == b ? 0 : a > b ? 1 : -1
-    })
-    res.send(logArray)
-})
-
-
 
 module.exports = logs;
