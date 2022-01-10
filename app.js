5db068addbc558479717170e931f09136f07346e
rae charles <r_charles87@yahoo.com>
2022-01-10 03:33:55 -0500

    stuck on status msg

--- app.js
+++ app.js
@@ -3,6 +3,23 @@ const app = express()
 
 const captainsLogs = require("./models/log.js")
 
+app.get("/", (req,res)=>{
+    if(req.url !== "/"){
+        res.status(301).send("trigger")
+        return;
+    }
+    res.send("Welcome to the Captains Log")
+})
+
+app.get("/logs", (req,res)=>{
+    // if(req.url !== "/logs"){
+    //     res.status(301).send("trigger")
+    //     return;
+    // }
+        res.send(captainsLogs)
+    
+
+})
 
 
 
