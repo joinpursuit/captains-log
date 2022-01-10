f3cc6d040ef449bfbe7a559587f7f35037d0c07f
stephanie lucano <stephaniemarielucano@gmail.com>
2022-01-10 00:50:21 -0500

    Adding the required 'app.listen(PORT, callback fn)' to listen to requests

--- server.js
+++ server.js
@@ -2,3 +2,7 @@ const app = require("./app")
 
 require("dotenv").config()
 const PORT = process.env.PORT
+
+app.listen(PORT, (request, response) => {
+    response.send("🚀 Listening on port ${PORT} 👩🏽‍🚀")
+})
