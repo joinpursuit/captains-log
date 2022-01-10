a41ee079746ebcf5d483d34e1d2034eb6d90d139
JoshMarte <joshuelm@gmail.com>
2022-01-10 09:11:37 -0500

    part one complete

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 // DEPENDENCIES
 const express = require("express");
+const res = require("express/lib/response");
 const app = express();
 const logsControllers = require("./controllers/logsControllers");
 
 // ROUTES
 app.get("/", (req, res) => {
-  res.send("Captains Log");
+  res.send("welcome to the captain's log");
 });
 
 app.use("/logs", logsControllers);
+
+// ERROR HANDLING
+app.get("*", (req, res) => {
+  res.status(404).redirect("https://github.com/joshmarte/captains-log-api");
+});
 
 // EXPORT
 module.exports = app;
--- controllers/logsControllers.js
+++ controllers/logsControllers.js
@@ -1,12 +1,83 @@
 // DEPENDENCIES
 const express = require("express");
-const logs = require("../models/logs");
+const logs = require("../models/log");
 const logsControllers = express.Router();
 
-// ROUTE
+// ROUTES
+
 logsControllers.get("/", (req, res) => {
-  res.json(logs);
+  const { order, mistakes, lastCrisis } = req.query;
+
+  switch (true) {
+    case lastCrisis === "gt10":
+      res.send(
+        logs.filter((item) => {
+          return item.daysSinceLastCrisis > 10;
+        })
+      );
+      break;
+    case lastCrisis === "gte20":
+      res.send(
+        logs.filter((item) => {
+          return item.daysSinceLastCrisis >= 20;
+        })
+      );
+      break;
+    case lastCrisis === "lte10":
+      res.send(
+        logs.filter((item) => {
+          return item.daysSinceLastCrisis <= 10;
+        })
+      );
+      break;
+    case mistakes === "true":
+      res.send(
+        logs.filter((item) => {
+          return item.mistakesWereMadeToday;
+        })
+      );
+      break;
+    case mistakes === "false":
+      res.send(
+        logs.filter((item) => {
+          return !item.mistakesWereMadeToday;
+        })
+      );
+      break;
+    case order === "asc":
+      res.send(
+        [...logs].sort((a, b) => {
+          var nameA = a.captainName.toUpperCase();
+          var nameB = b.captainName.toUpperCase();
+
+          return nameA < nameB ? -1 : 1;
+        })
+      );
+      break;
+    case order === "desc":
+      res.send(
+        [...logs].sort((a, b) => {
+          var nameA = a.captainName.toUpperCase();
+          var nameB = b.captainName.toUpperCase();
+
+          return nameA < nameB ? 1 : -1;
+        })
+      );
+      break;
+    default:
+      res.json(logs);
+      break;
+  }
 });
+//    PART ONE BONUS
+/*/logs?order=asc it will organize the logs alphabetically
+/logs?order=desc it will organize the logs in reverse alphabetical order
+*/
+
+// logsControllers.get("/:id",(req,res)=>{
+//   const {id} = req.params
+//   res.json(logs[id] ? logs[id] : )
+// })
 
 // EXPORT
 module.exports = logsControllers;
