f5a0ab47195c16f299f63e253162cae2ecb377ad
Rose Durandisse <rosedurandisse@pursuit.org>
2022-01-10 15:56:06 -0500

    Added a home page and a logs page

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 const express = require("express");
 const app = express();
+const logs=require('./models/log')
 
+// app.use('/log',logTestController)
 
+app.get('/', (request,response)=> {
+    response.send("Welcome to the captain's log"
+    )
+})
+
+app.get('/logs', (request,response)=> {
+    response.send(logs)
+})
+
+app.get('*',(request,response)=> {
+    response.status(404)
+    .json({error:'Page not found'})
+})
 module.exports= app;
