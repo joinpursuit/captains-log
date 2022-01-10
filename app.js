2ad4370799c3b6d21774e1a2081eecf1dc48f4de
stephanie lucano <stephaniemarielucano@gmail.com>
2022-01-10 01:23:48 -0500

    Import ./models/log.js to app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const app = express()
+const logsArray = require("./models/log") 
 
 app.get("/", (request, response) => {
     response.send("Welcome to the Captain's Log App")
