8a6ce2344a3d569cc59ff75c0b83b3ed4bcaf17c
Lakisha J <lakishajohnson@pursuit.org>
2022-01-11 16:37:27 -0500

    Added middleware to parse incoming requests with JSON files

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const logController = require("./controllers/logs")
 const app = express();
 
+app.use(express.json())
 app.use("/logs", logController);
 // HOME ROUTE
 app.get("/", (req, res) => {
