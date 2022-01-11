2a2f72d0628cd7d8be5802efebf08454147027c1
Lakisha J <lakishajohnson@pursuit.org>
2022-01-10 22:46:33 -0500

    PsuedoCoded file

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const logController = require("./controllers/logs")
 const app = express();
 
 app.use("/logs", logController);
-
+// HOME ROUTE
 app.get("/", (req, res) => {
     console.log("Get request to /")
     res.send("Welcome to the captain's log")
 });
-
+// ERROR HANDLER 
 app.get("*", (req, res) => {
     res.status(404).json({error: "Page not found"})
 });
