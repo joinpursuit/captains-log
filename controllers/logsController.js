b3cb0c3fe646200bf6c112051d580879377bf77e
Laiba Sajid Khan <sajid.laiba378@gmail.com>
2022-01-12 16:48:21 -0500

    order req.query bonus completed

--- controllers/logsController.js
+++ controllers/logsController.js
@@ -1,34 +1,71 @@
 const express = require("express");
 const logsRoute = express.Router();
 const logsArray = require("../models/log.js");
-
+let logsArrayCopy = logsArray.slice();
+// /logs?order=asc it will organize the logs alphabetically
+// /logs?order=desc it will organize the logs in reverse alphabetical order
+// /logs?mistakes=true it will only show the logs where the value of mistakesWereMadeToday is true
+// /logs?mistakes=false it will only show the logs where the value of mistakesWereMadeToday is false
+// /logs?lastCrisis=gt10 it will return all the logs where the daysSinceLastCrisisis greater tthan 10
+// /logs?lastCrisis=gte20it will return all the logs where the daysSinceLastCrisisis greater tthan or equal to 20
+// /logs?lastCrisis=lte5it will return all the logs where the daysSinceLastCrisisis less tthan or equal to 5
 logsRoute.get("/", (req, res)=> {
   const { order, mistakes, lastCrisis } = req.query;
 
-  switch( order ){
-    case "asc":
-      break;
-    case "desc":
-      break;
-    default:
+  console.log("~~req.query~~", req.query);
+
+  if( order || mistakes || lastCrisis ) {
+    switch( order ){
+      case "asc":
+        logsArrayCopy.sort((a, b)=> {
+          let first = a.captainName.toLowerCase();
+          let second = b.captainName.toLowerCase();
+          if( first < second ){
+            return -1;
+          } else {
+            return 1;
+          }
+        });
+        res.json(logsArrayCopy);
+        break;
+      case "desc":
+        logsArrayCopy.sort((a, b)=>{
+          let first = a.captainName.toLowerCase();
+          let second = b.captainName.toLowerCase();
+          if ( first === second ){
+            return 0;
+          } else  if ( first < second ){
+            return 1;
+          } else {
+            return -1;
+          }
+        });
+        res.json(logsArrayCopy);
+        break;
+      default:
+    }
+  
+    switch( mistakes ){
+      case "true":
+        break;
+      case "false":
+        break;
+      default:
+    }
+  
+    switch( lastCrisis ){
+      case "gt10":
+        break;
+      case "gte20":
+        break;
+      case "lte5":
+        break;
+      default:
+    }
+  } else {
+    res.json(logsArray);
   }
-  switch( mistakes ){
-    case "true":
-      break;
-    case "false":
-      break;
-    default:
-  }
-  switch( lastCrisis ){
-    case "gt10":
-      break;
-    case "gte20":
-      break;
-    case "lte5":
-      break;
-    default:
-  }
-  res.json(logsArray);
+
 });
 
 logsRoute.get("/:id", (req, res) => {
