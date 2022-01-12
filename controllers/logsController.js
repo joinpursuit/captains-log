fdbe47f8e9c695303552c24bc97aaa76bad5333f
imolina212 <86175612+imolina212@users.noreply.github.com>
2022-01-11 22:25:41 -0500

    sends the corresponding log when a valid index is given

--- controllers/logsController.js
+++ controllers/logsController.js
@@ -6,4 +6,18 @@ log.get("/", (req, res) => {
   res.json(logArray);
 });
 
+log.get("/:id", (request, response) => {
+  if(logArray[request.params.id]) {
+    response.send(logArray[request.params.id])
+  } else {
+    response.status(404).json({ error: "Resource not found" });
+  }
+})
+
+log.post("/", (request, response) => {
+  console.log("POST to /log")
+  logArray.push(request.body)
+  response.json(logArray)
+})
+
 module.exports = log;
