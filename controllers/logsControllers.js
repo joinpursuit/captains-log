5ce573a65accac578d9f9a5ede481d355ec6f8b8
justin <justin.d.diaz@gmail.com>
2022-01-12 12:46:10 -0500

    all test pass

--- controllers/logsControllers.js
+++ controllers/logsControllers.js
@@ -12,7 +12,7 @@ logs.get("/:index", (request, response) => {
   if (logsArray[index]) {
     response.json(logsArray[index]);
   } else {
-    response.status(404).json({ error: "Resource not found" });
+    response.redirect(404).json({ error: "Resource not found" });
   }
 });
 
@@ -20,6 +20,27 @@ logs.post("/", (request, response) => {
   console.log("POST to /logs");
   logsArray.push(request.body);
   response.status(201).json(logsArray);
+});
+
+logs.delete("/:index", (request, response) => {
+  const { index } = request.params;
+  if (logsArray[index]) {
+    const deletedLog = logsArray.splice(index, 1)[0];
+    response.status(200).json(deletedLog);
+  } else {
+    response.status(404).json({ error: "log Not Found" });
+  }
+});
+
+logs.put("/:index", (request, response) => {
+  const { index } = request.params;
+  if (logsArray[index]) {
+    logsArray[index] = request.body;
+    // logsArray.splice(index, 1, request.body);
+    response.status(200).json(logsArray);
+  } else {
+    response.status(404).json({ error: "log Not Found" });
+  }
 });
 
 module.exports = logs;
