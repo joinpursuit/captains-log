7ba99c43190c4f0c677580d8d8cddae9e8aadcc0
Laiba Sajid Khan <sajid.laiba378@gmail.com>
2022-01-12 15:46:45 -0500

    redirect completed, working on query string bonus activity

--- app.js
+++ app.js
@@ -5,11 +5,16 @@ const app = express();
 app.use(express.json());
 
 const logsController = require("./controllers/logsController.js");
+const logsArray = require("./models/log.js");
 
 app.get("/", (req, res)=> {
     res.send(`welcome to the captain's log`);
 });
 
 app.use("/logs", logsController);
+
+app.get("*", (req, res)=> {
+    res.status(404).json(logsArray);
+});
 
 module.exports = app;
--- controllers/logsController.js
+++ controllers/logsController.js
@@ -1,12 +1,48 @@
 const express = require("express");
 const logsRoute = express.Router();
-const logsData = require("../models/logs.js");
+const logsArray = require("../models/log.js");
 
 logsRoute.get("/", (req, res)=> {
-  res.json(logsData);
+  const { order, mistakes, lastCrisis } = req.query;
+
+  switch( order ){
+    case "asc":
+      break;
+    case "desc":
+      break;
+    default:
+  }
+  switch( mistakes ){
+    case "true":
+      break;
+    case "false":
+      break;
+    default:
+  }
+  switch( lastCrisis ){
+    case "gt10":
+      break;
+    case "gte20":
+      break;
+    case "lte5":
+      break;
+    default:
+  }
+  res.json(logsArray);
 });
 
+logsRoute.get("/:id", (req, res) => {
+  const { id } = req.params;
+  if( logsArray[id] ) {
+    res.send(logsArray[id]);
+  } else {
+    res.redirect("/")
+  }
+});
 
+//POST
+
+//DELETE
 
 
 module.exports = logsRoute;
