fcedf51299e8ebe6306f381f3fb5bb4c637acb1d
Jalamang <Jalamangjanka@gmail.com>
2022-01-12 22:00:56 -0500

    more stylings

--- v2/controllers/logsController.js
+++ v2/controllers/logsController.js
@@ -1,32 +1,19 @@
 //Dependencies
 const express = require("express");
-// const  engine  = require("express-handlebars");
 
 //files
 const captainLogArray = require("../../models/logs");
-console.log(captainLogArray);
+
 //.Router creates a new controller that handles a sub-routes.
 const v2 = express.Router();
 
-// Handlebars Middleware
-// app.engine("handlebars", engine.engine({ defaultLayout: "main" }));
-// app.set("view engine", "handlebars");
-
-// v2.get("/:index", (request, response) => {
-//   const { index } = request.params;
-//   if (captainLogArray[Number(index)]) {
-//     captainLogArray.map(el => {
-//       if(el.captainName === captainLogArray[Number(index)]) {
-//         response.send(el.captainName);
-//       }
-//     });
-//   }
-
-// });
 
 v2.get("/:index", (request, response) => {
   const { index } = request.params;
   console.log("GET request /v2/logs" + index);
+
+  if(index > captainLogArray.length-1) response.status(404).send({error: 'Array Index Out Of Bounds Exception'})
+
   if (captainLogArray[Number(index)].captainName) {
     let html = "",
       anchor = "",
@@ -36,7 +23,7 @@ v2.get("/:index", (request, response) => {
         response.format({
           "text/html": () => {
             anchor = "<a>";
-            html = "<ul>";
+            html = "<ul style='border-collapse: separate; border:2px solid #ff9900;'>";
             button = "<button>";
             html += `<h4> Captain Name: ${el.captainName} </h4>`;
             html += `<h1> Title: ${el.title} </h1>`;
@@ -45,15 +32,14 @@ v2.get("/:index", (request, response) => {
             html += `<p> daysSinceLastCrisis: ${el.daysSinceLastCrisis}</p>`;
             html += "</ul>";
             anchor = `<a href=${"/logs"} onmouseover="this.style.backgroundColor='#ffdd00';this.style.color='#aaaaaa'">Visit captain log API</a>`;
-            button = `<p><button><a href=${"/v2/logs"}>Back</a> </button><p>`;
+            button = `<p><button style='border-collapse: separate;'><a href=${"/v2/logs"}>Back</a> </button><p>`;
           },
         });
       }
     });
     response.send(html + anchor + button);
-  } else {
-    response.status(406).send("Not Acceptable");
-  }
+  } 
+  
 });
 
 module.exports = v2;
