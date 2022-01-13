0c206e97806b2e088246787ff6f871470506c1c4
imolina212 <86175612+imolina212@users.noreply.github.com>
2022-01-12 19:58:04 -0500

    added middleware

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const app = express();
 const logsController = require("./controllers/logsController.js");
+// Middleware (new way of writing body-parser)
+app.use(express.json());
+
+app.use("/logs", logsController);
 
 app.get("/", (req, res) => {
   res.send(`welcome to the captain's log`)
 })
-
-app.use("/logs", logsController);
 // 404 PAGE
 app.get("*", (req, res) => {
   res.status(404).json({ error: "Page not found" });
