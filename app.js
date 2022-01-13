545d5d3dfe7b04a23bc91ccadd6ed9f14a5ee618
Tashawnee Guarriello <tashawneeguarriello@pursuit.org>
2022-01-12 20:08:54 -0500

    Added 404 status/Starting the Post

--- app.js
+++ app.js
@@ -11,4 +11,8 @@ app.get("/", (req, res) => {
   res.send("Welcome to the Captain's Log");
 });
 
+app.get("*", (req, res) => {
+  res.status(404).json({ error: "Page not found" });
+});
+
 module.exports = app;
--- controllers/logsController.js
+++ controllers/logsController.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { append } = require("express/lib/response");
 
 const logsArray = require("../models/log");
 
@@ -7,6 +8,21 @@ const logs = express.Router();
 logs.get("/", (req, res) => {
   console.log("GET request to / logs");
   res.send(logsArray);
+});
+
+logs.post("/", (req, res) => {});
+
+logs.get("/:arrayIndex", (req, res) => {
+  console.log("GET request received to route: /logs/:arrayIndex");
+  const arrayIndex = req.params.arrayIndex;
+  res.json(logsArray[arrayIndex]);
+});
+
+//req.params.id === :id
+
+logs.delete("/:id", (req, res) => {
+  logsArray.splice(req.params.id, 1);
+  res.status(200).json(logsArray);
 });
 
 module.exports = logs;
