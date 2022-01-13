ebac5f73477fe04699a53fcd11fc049f642421a7
hinkabi <hinkabi@gmail.com>
2022-01-12 21:34:07 -0500

    Passing all tests

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+const logController = require("./controllers/logController.js");
+
+app.get("/", (req, res)=>{
+    res.send("Welcome to the captains log")
+});
+
+app.use("/logs", logController);
+
+app.get("*", (req, res) =>{
+    res.status(404).json({ error: "Page not found" })
+})
+
+module.exports = app;
