1d384d5003d570820c00a6378096316dc28e4354
Melissa Lopez <1zuzsuit492@Melissas-MacBook-Pro.local>
2022-01-12 21:32:56 -0500

    passing 5 out of 6

--- controllers/logsController.js
+++ controllers/logsController.js
@@ -1,3 +1,4 @@
+const { request } = require("express");
 const express = require("express");
 
 const logsArray = require("../models/log");
@@ -19,9 +20,35 @@ logs.post("/", (request, response) => {
   //updating the log.js [logsArray] to json
 });
 
+//destructured index and set it equal to request.params
+//.params already exists in request
+//accesses any value after colon (:), i.e. /logs/2
+//sending a response to get the index from array
 logs.get("/:index", (request, response) => {
   const { index } = request.params;
   response.send(logsArray[index])
 })
+
+logs.get('/:index', (request, response) => {
+  const { index } = request.params;
+  if(logsArray[index]){
+    response.json(logsArray[index])
+  }
+  else{
+    response.redirect('/', 302)
+  }
+})
+
+logs.delete("/:index", (request, response) => {
+  const { index } = request.params;
+  if (logsArray[index]) {
+    const deleted = logsArray.splice(index, 1)[0];
+    response.status(200).json(deleted);
+  } else {
+    response.status(404).json({ error: "log Not Found" });
+  }
+});
+
+
 
 module.exports = logs;
