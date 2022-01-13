97ff282c335a466adb3ce3bb980b16eeecfda015
james russell <jamesrussell@pursuit.org>
2022-01-13 17:07:06 -0500

    1 test failing, working on it

--- controllers/logsController.js
+++ controllers/logsController.js
@@ -7,10 +7,37 @@ const logs = express.Router();
 const logsArr = require("../models/log.js");
 
 // routes
+// sends the logs array
 logs.get("/", (req, res) => {
-    res.json(logsArr);
+  res.json(logsArr);
 });
 
-// logs.get("*", (req, res) => {});
+// adds new log to end of logs array
+logs.post("/", (req, res) => {
+  logsArr.push(req.body);
+  res.json(logsArr[logsArr.length - 1]);
+});
+
+// sends the corresponding log when a valid index is given
+// sends a redirect when an invalid index is given
+logs.get("/:index", (req, res) => {
+  const { index } = req.params;
+  if (logsArr[index]) {
+    res.json(logsArr[index]);
+  } else {
+    // redirect wildcard
+    res.redirect("*");
+  }
+});
+
+// deletes at the index in the logs array
+logs.delete("/:index", (req, res) => {
+  const { index } = req.params;
+  if (logsArr[index]) {
+    res.json(logsArr.splice(index, 1));
+  } else {
+    res.redirect("*");
+  }
+});
 
 module.exports = logs;
