48df7e1ed6d05c0f9dd420a86ce8982db650d393
stephanie-lucano <stephaniemarielucano@pursuit.org>
2022-01-14 17:29:25 -0500

    Imported cors and invoked the cors function

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express")
 const app = express()
 const logsController = require("./controllers/logsController")
+const cors = require("cors")
 
+app.use(cors())
 app.use(express.json())
 app.use("/logs", logsController)
 
