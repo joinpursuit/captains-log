94b782ccb4898cacbb69b71257073426f8e04826
justin <justin.d.diaz@gmail.com>
2022-01-15 18:00:34 -0500

    cors

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const logsControllers = require("./controllers/logsControllers");
 const express = require("express");
+const cors = require("cors");
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/logs", logsControllers);
 
