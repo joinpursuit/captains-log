abf5ea18287255eb03bcc878e6fbd6b78b7c641c
Lakisha J <lakishajohnson@pursuit.org>
2022-01-15 15:29:31 -0500

    Small changes

--- app.js
+++ app.js
@@ -1,26 +1,22 @@
 const express = require("express");
-const logController = require("./controllers/logs")
-const v2Controller = require("./v2/controllers/logsController")
+const logController = require("./controllers/logs");
+const v2Controller = require("./v2/controllers/logsController");
 const app = express();
+const cors = require("cors");
 
-app.use(express.json())
+app.use(express.json());
+app.use(cors());
 app.use("/logs", logController);
 app.use("/v2/logs", v2Controller);
 
 // HOME ROUTE
 app.get("/", (req, res) => {
-    console.log("Get request to /")
-    res.send("Welcome to the captain's log")
+  console.log("GET request to /");
+  res.send("Welcome to the captain's log");
 });
-// ERROR HANDLER 
-// app.get("*", (req, res) => {
-//     res.status(404).json({error: "Page not found"})
-// });
+// ERROR HANDLER
 app.get("*", (request, response) => {
-    response.status(400).send(`<img src="https://http.cat/400">`);
+  response.status(401).send(`<img src="https://http.cat/400">`);
 });
-
-
-
 
 module.exports = app;
--- controllers/logs.js
+++ controllers/logs.js
@@ -5,60 +5,62 @@ const logArray = require("../models/log");
 
 // HOME ROUTE
 logs.get("/", (req, res) => {
-  console.log("Get request to /logs");
-//   console.log(req.query);
+  console.log("GET request to /logs");
+  //   console.log(req.query);
   const { order, mistakes, lastCrisis } = req.query;
   if (req.originalUrl === "/logs") {
     res.send(logArray);
   } else {
     const arr = [...logArray];
-    // HOME ROUTE W/QUERY FOR ARRAY IN ASCENDING ORDER: /logs?order=asc 
+    // HOME ROUTE W/QUERY FOR ARRAY IN ASCENDING ORDER: /logs?order=asc
     if (order === "asc") {
       arr.sort((a, b) => {
         (a = a.captainName.toUpperCase()), (b = b.captainName.toUpperCase());
         return a === b ? 0 : a > b ? 1 : -1;
       });
       res.send(arr);
-    // HOME ROUTE W/QUERY FOR ARRAY IN DESCENDING ORDER: /logs?order=desc
+      // HOME ROUTE W/QUERY FOR ARRAY IN DESCENDING ORDER: /logs?order=desc
     } else if (order === "desc") {
-    //    console.log(order);
+      //    console.log(order);
       arr.sort((a, b) => {
         (a = a.captainName.toUpperCase()), (b = b.captainName.toUpperCase());
         return a === b ? 0 : a > b ? -1 : 1;
       });
       res.send(arr);
-    //  HOME ROUTE W/QUERY IF MistakesMadeToday IS TRUE: /logs?mistakes=true
+      //  HOME ROUTE W/QUERY IF MistakesMadeToday IS TRUE: /logs?mistakes=true
     } else if (mistakes === "true") {
-      const allMistakes = arr.filter((el) => el.mistakesWereMadeToday ===  true) 
-     console.log(allMistakes);
-        res.send(allMistakes);
-    } 
+      const allMistakes = arr.filter((el) => el.mistakesWereMadeToday === true);
+      //  console.log(allMistakes);
+      res.send(allMistakes);
+    }
     // HOME ROUTE W/QUERY IF MistakesMadeToday IS FALSE:/logs?mistakes=false
     else if (mistakes === "false") {
-          const allMistakes = arr.filter((el) => el.mistakesWereMadeToday ===  false) 
-        //  console.log(allMistakes);
-            res.send(allMistakes);
-        }
-        // HOME ROUTE W/QUERY IF DaysSinceCrisis is > 10: /logs?lastCrisis=GreaterThan10
-        else if (lastCrisis === "gt10") {
-            const crisisDays = arr.filter((el) => el.daysSinceLastCrisis > 10 ) 
-              res.send(crisisDays);
-          }
-        // HOME ROUTE W/QUERY IF DaysSinceCrisis is >= 10:  /logs?lastCrisis=GreaterThanOrEqualTo20
-          else if (lastCrisis === "gte20") {
-            const crisisDays = arr.filter((el) => el.daysSinceLastCrisis >= 20 ) 
-              res.send(crisisDays);
-          }
-        // HOME ROUTE W/QUERY IF DaysSinceCrisis is <= 5: /logs?lastCrisis=LessThanOrEqualTo5
-          else if (lastCrisis === "lte5") {
-            const crisisDays = arr.filter((el) => el.daysSinceLastCrisis <= 5 ) 
-              res.send(crisisDays);
-          }
-}
+      const allMistakes = arr.filter(
+        (el) => el.mistakesWereMadeToday === false
+      );
+      //  console.log(allMistakes);
+      res.send(allMistakes);
+    }
+    // HOME ROUTE W/QUERY IF DaysSinceCrisis is > 10: /logs?lastCrisis=GreaterThan10
+    else if (lastCrisis === "gt10") {
+      const crisisDays = arr.filter((el) => el.daysSinceLastCrisis > 10);
+      res.send(crisisDays);
+    }
+    // HOME ROUTE W/QUERY IF DaysSinceCrisis is >= 10:  /logs?lastCrisis=GreaterThanOrEqualTo20
+    else if (lastCrisis === "gte20") {
+      const crisisDays = arr.filter((el) => el.daysSinceLastCrisis >= 20);
+      res.send(crisisDays);
+    }
+    // HOME ROUTE W/QUERY IF DaysSinceCrisis is <= 5: /logs?lastCrisis=LessThanOrEqualTo5
+    else if (lastCrisis === "lte5") {
+      const crisisDays = arr.filter((el) => el.daysSinceLastCrisis <= 5);
+      res.send(crisisDays);
+    }
+  }
 });
 // HOME ROUTE W/PARAMETER POINTING TO INDEX POSITION, IF NO POSITION REDIRECT TO ERROR ROUTE
 logs.get("/:arrayIndex", (req, res) => {
-  console.log("Get request to /arrayIndex");
+  console.log("GET request to /arrayIndex");
   const { arrayIndex } = req.params;
   if (logArray[Number(arrayIndex)]) {
     res.send(logArray[Number(arrayIndex)]);
@@ -68,25 +70,27 @@ logs.get("/:arrayIndex", (req, res) => {
 });
 // POST/CREATE NEW LOG AND PUSH INTO ARRAY, NEW ARRAY WILL BE IN JSON FORMAT
 logs.post("/", (req, res) => {
-    console.log("/POST to /logs");
-    logArray.push(req.body);
-    res.json(logArray);
-    res.status(201);
-  });
+  console.log("/POST to /logs");
+  logArray.push(req.body);
+  res.json(logArray);
+  res.status(201);
+});
 // DELETES ENTIRE OBJECT AT INDEX POSITION
-  logs.delete("/:index", (req, res) => {
-    const { index } = req.params;
-    if (logArray[index]) {
-      const deletedLog = logArray.splice(index, 1);
-      res.status(200).json(deletedLog);
-    } else {
-        res.status(404).json({error: "Log not found"})
-    }
-  });
+logs.delete("/:index", (req, res) => {
+  console.log(`DELETE to /logs/${req.params.index}`);
+  const { index } = req.params;
+  if (logArray[index]) {
+    const deletedLog = logArray.splice(index, 1);
+    res.status(200).json(deletedLog);
+  } else {
+    res.status(404).json({ error: "Log not found" });
+  }
+});
 // UPDATES OBJECT AT INDEX POSITION BY REPLACING THAT ENTIRE OBJECT
-  logs.put("/:index", (req, res) => {
-      logs[req.params.index] = req.body
-      res.status(200).json(logArray[req.params.index])
-  })
+logs.put("/:index", (req, res) => {
+  console.log(`PUT to /logs/${req.params.index}`);
+  logs[req.params.index] = req.body;
+  res.status(200).json(logArray[req.params.index]);
+});
 
 module.exports = logs;
