72c5d8e88ea860bd06227e55aa3ba08838224f25
Jenna Qian <jiayin1996@gmail.com>
2022-01-16 17:52:37 -0500

    Show & Create

--- controllers/logsController.js
+++ controllers/logsController.js
@@ -6,4 +6,18 @@ logs.get("/", (req, res) => {
   res.json(logsArray);
 });
 
+logs.get("/:index", (req, res) => {
+  const { index } = req.params;
+  if (logsArray[index]) {
+    res.json(logsArray[index]);
+  } else {
+    res.status(404).json({ error: "Not Found" });
+  }
+});
+
+logs.post("/", (req, res) => {
+  logsArray.push(req.body);
+  res.json(logsArray[logsArray.length - 1]);
+});
+
 module.exports = logs;
