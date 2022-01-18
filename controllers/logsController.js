2b6eee302eae535e74f2ead787221991b03cafb3
Christina Loiacono <loiaconochristina@gmail.com>
2022-01-18 10:51:13 -0500

    added back in bonus section for part 1. fixed code.

--- controllers/logsController.js
+++ controllers/logsController.js
@@ -1,44 +1,84 @@
 const express = require("express");
 const req = require("express/lib/request");
-const log = express.Router();
+const logRoutes = express.Router();
 const logArr = require("../models/log.js");
 
-log.get("/", (req, res) => {
-  res.json(logArr);
+logRoutes.get("/", (req, res) => {
+     // *** START bonus section - Part 1
+     const { order, mistakes, lastCrisis } = req.query;
+     if (order === "asc") {
+         logArr.sort((a, b) => (a.title > b.title) ? 1 : -1)
+     }
+     if (order === "desc") {
+         logArr.sort((a, b) => (a.title < b.title) ? 1 : -1)
+     }
+     if (mistakes === "true") {
+         const result = logArr.filter((post)=>{
+             return post.mistakesWereMadeToday === true;
+         })
+        res.json(result);
+     }
+     if (mistakes === "false") {
+         const result = logArr.filter((post)=>{
+             return post.mistakesWereMadeToday === false;
+         })
+        res.json(result);
+     }
+     if (lastCrisis === "gt10") {
+         const result = logArr.filter((post)=>{
+             return post.daysSinceLastCrisis > 10;
+         })
+        res.json(result);
+     }
+     if (lastCrisis === "gte20") {
+         const result = logArr.filter((post)=>{
+             return post.daysSinceLastCrisis >= 20;
+         })
+        res.json(result);
+     }
+     if (lastCrisis === "lte5") {
+         const result = logArr.filter((post)=>{
+             return post.daysSinceLastCrisis <= 5;
+         })
+        res.json(result);
+     }
+     // *** END bonus section - Part 1
+ 
+    res.json(logArr);
 });
 //GET
-log.get("/:id", (req, res) => {
-  if(logArr[req.params.id]) {
-    res.send(logArr[req.params.id])
-  } else {
-    res.redirect("/logs/:id")
-  }
+logRoutes.get("/:id", (req, res) => {
+    if(logArr[req.params.id]) {
+        res.send(logArr[req.params.id])
+    } else {
+        res.redirect("/logs/:id")
+    }
 })
-//POST
-log.post("/", (req, res) => {
-  logArr.push(req.body)
-  res.json(logArr[logArr.length - 1])
+//CREATE (POST)
+logRoutes.post("/", (req, res) => {
+    logArr.push(req.body)
+    res.json(logArr[logArr.length - 1])
 })
 
-//PUT
-log.put("/:id", (req, res) => {
-  const { id } = req.params
-  if (logArr[id]) {
-    logArr[id] = req.body;
-    res.status(200).json(logArr[id])
-  } else {
-    res.status(404).json({error: "Not found"})
-  }
+//UPDATE (PUT)
+logRoutes.put("/:id", (req, res) => {
+    const { id } = req.params
+    if (logArr[id]) {
+        logArr[id] = req.body;
+        res.status(200).json(logArr[id])
+    } else {
+        res.status(404).json({error: "Not found"})
+    }
  })
 
 // DELETE
-log.delete("/:id", (req, res) => {
-  if (logArr[req.params.id]) {
-    const deletedLog = logArr.splice(req.params.id, 1);
-    res.status(200).json(deletedLog);
-  } else {
-    res.status(404).json({ error: "Not found" });
-  }
+logRoutes.delete("/:id", (req, res) => {
+    if (logArr[req.params.id]) {
+        const deletedLog = logArr.splice(req.params.id, 1);
+        res.status(200).json(deletedLog);
+    } else {
+        res.status(404).json({ error: "Not found" });
+    }
 });
 
-module.exports = log;
+module.exports = logRoutes;
