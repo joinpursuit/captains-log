6c531ac69dbb0a427570b0c9533cabd1bb6a33dc
Sara D'Arce <saradarce@gmail.com>
2022-01-18 00:02:44 -0500

    Done but still need alot of help

---
+++ controllers/logsController.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const logsArray = require("../models/log.js");
+
+const logs = express.Router();
+
+logs.get("/", (_, response) => {
+  response.json(logsArray);
+});
+
+logs.post("/", (request, response) => {
+  response.json(logsArray.push(request.body));
+});
+
+logs.get("/:index", (request, response) => {
+  const { index } = request.params;
+  logsArray[index] ? response.json(logsArray[index]) : response.redirect("/");
+});
+
+logs.delete("/:index", (request, response) => {
+  const { index } = request.params;
+  logsArray[index]
+    ? response.json(logsArray.splice(index, 1))
+    : response.status(404).json({ error: "index not found" });
+});
+
+module.exports = logs;
