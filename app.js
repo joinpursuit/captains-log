556fe706a35e75ff01d943a3a1794d17e643f7d9
“Lili <lilyhuang114classb@gmail.com>
2022-01-20 20:27:33 -0500

    Updated.

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const logsController = require("./controllers/logsController");
 
+app.use(cors());
 
 app.use(express.json());
 
