eaa2a98835e0756c585e664c321cdfd6298fc5fa
meera-ramesh19 <meeraramesh19@outlook.com>
2022-07-15 21:33:24 -0400

    bonus attempted

--- controllers/logs.js
+++ controllers/logs.js
@@ -1,54 +1,67 @@
-const express = require("express")
-const logsArray = require("../models/log")
-const logs = express.Router()
+const express = require('express');
+const logsArray = require('../models/log');
+const logs = express.Router();
 const { validateURL } = require('../models/validations.js');
+const {
+  sortByName,
+  filterMistakes
+
+} = require('../helper/bonus.js');
 
 logs.use(express.json());
 
-
-
 logs.get('/', (req, res) => {
-    console.log("Get /logs")
-    res.send(logsArray) 
-})
+  const { order, mistakes, lastCrisis } = req.query;
+  //make a copy of the logsArray so we do not manipulate the original array
+  let logArrayCopy = [...logsArray];
+
+  if (order) {
+    logArrayCopy = sortByName(logArrayCopy, order);
+  }
+
+  if (mistakes !== undefined || mistakes !== null) {
+    logArrayCopy = filterMistakes(logArrayCopy, mistakes);
+  }
+
+  res.send(logArrayCopy);
+});
 
 logs.get('/:id', (req, res) => {
-    const id = parseInt(req.params.id);
-    if (id < 0 || id >= logsArray.length) {
-      res.redirect(404);
-      return;
-    } else {
-      res.json(logsArray[id]);
-    }
-  });
-
+  const id = parseInt(req.params.id);
+  if (id < 0 || id >= logsArray.length) {
+    res.redirect(404);
+    return;
+  } else {
+    res.json(logsArray[id]);
+  }
+});
 
 // CREATE
 logs.post('/', (req, res) => {
-    logsArray.push(req.body);
-    res.send(logsArray[logsArray.length - 1]);
-  });
+  logsArray.push(req.body);
+  res.send(logsArray[logsArray.length - 1]);
+});
 
-  // UPDATE
+// UPDATE
 logs.put('/:arrayIndex', validateURL, async (req, res) => {
-    if (logsArray[req.params.arrayIndex]) {
-      logsArray[req.params.arrayIndex] = req.body;
-      res.status(200).json(logsArray[req.params.arrayIndex]);
-    } else {
-      res.status(404).json({ error: 'Not found' });
-    }
-  });
+  if (logsArray[req.params.arrayIndex]) {
+    logsArray[req.params.arrayIndex] = req.body;
+    res.status(200).json(logsArray[req.params.arrayIndex]);
+  } else {
+    res.status(404).json({ error: 'Not found' });
+  }
+});
 
-  //DELETE
+//DELETE
 
 logs.delete('/:arrayIndex', (req, res) => {
-    if (logsArray[req.params.arrayIndex]) {
-      const deletedlog = logsArray.splice(req.params.indexArray, 1);
-      console.log(deletedlog);
-      res.status(200).json(deletedlog - 1);
-    } else {
-      res.status(404).json({ error: 'Not found' });
-    }
-  });
+  if (logsArray[req.params.arrayIndex]) {
+    const deletedlog = logsArray.splice(req.params.indexArray, 1);
+    console.log(deletedlog);
+    res.status(200).json(deletedlog - 1);
+  } else {
+    res.status(404).json({ error: 'Not found' });
+  }
+});
 
-  module.exports=logs
+module.exports = logs;
