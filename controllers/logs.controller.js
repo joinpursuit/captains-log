1b67de42aabc0095c2d883802d116db31486be3e
Jorge Carrera <JCarrera994@gmail.com>
2022-07-17 17:52:52 -0400

    adds GET and POST functionality to lab

---
+++ controllers/logs.controller.js
@@ -0,0 +1,24 @@
+express = require('express'); 
+const logs = express.Router();
+const logsData = require('../models/log')
+
+logs.get('/', (req, res) => {
+  res.json(logsData)
+})
+
+logs.get('/:arrayIndex', (req, res) => {
+  const { arrayIndex } = req.params; 
+  if(logsData[arrayIndex]) {
+    res.json(logsData[arrayIndex])
+  } else {
+    res.status(404).redirect('/Error')
+  }
+})
+
+logs.post('/', (req, res) => {
+  logsData.push(req.body); 
+  res.json(logsData[logsData.length-1])
+})
+
+
+module.exports = logs
