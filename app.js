cc29746d4aed7c9af49c6f0528441fe2e66e73ac
EmaleeSoto <emaleesoto@gmail.com>
2022-07-17 03:42:31 -0400

    Passed logs array index, get logs id and redirects error

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const app = express();
+const logs = require("./models/log.js");
 
 const logsController = require("./controllers/logs.controller.js");
 app.use("/logs", logsController);
 
 app.get("/", (req, res) => {
   res.send("welcome to the captain's log");
+});
+app.get("/logs/:id", (req, res) => {
+  const { id } = req.params;
+  if (logs[id]) {
+    res.json(logs[id]);
+  } else {
+    res.redirect(404).json({ error: "Not found" });
+  }
 });
 
 module.exports = app;
