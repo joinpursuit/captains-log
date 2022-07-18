885cd0bf01c74ebdeb6c37d93dbdf17467aa5370
H.C.Guerrero <clydeguerrero1@gmail.com>
2022-07-18 20:58:04 -0300

    5/6 tests passed.

--- controllers/log-controller.js
+++ controllers/log-controller.js
@@ -12,5 +12,21 @@ router.post('/', (request, response) => {
   captainLogs.push(request.body);
   response.json(captainLogs[captainLogs.length - 1]);
 });
+router.get('/:arrayIndex', (request, response) => {
+  if (captainLogs[request.params.arrayIndex]) {
+    response.json(captainLogs[request.params.arrayIndex]);
+  } else {
+    response.status(403).send('Not a valid Index!');
+  }
+});
+router.delete('/:arrayIndex', (request, response) => {
+  const { arrayIndex } = request.params;
+  if (!captainLogs[arrayIndex]) {
+    response.send('Not a valid index number');
+  } else {
+    captainLogs.splice(arrayIndex, 1);
+    response.send(captainLogs);
+  }
+});
 
 module.exports = router;
