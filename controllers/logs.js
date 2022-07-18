b6bd1fb8ce7ca4d03369dc6f61adf2c1a33cfa45
T.K <nique@Niques-MacBook-Air.local>
2022-07-18 15:21:46 -0400

    adding DELETE

--- controllers/logs.js
+++ controllers/logs.js
@@ -7,18 +7,23 @@ logs.get("/", (req, res) => {
   res.json(logsData);
 });
 
-logs.get("/:arrayIndex", (req, res) => {
-    const { arrayIndex } = req.params;
-    if (logsData[arrayIndex]) {
-        res.json(logsData[arrayIndex])
-    } else {
-        res.status(404).send("page not found")
-    }
-})
-
 logs.post("/", (req, res) => {
-    logsData.push(req.body);
-    res.json()
-})
+  logsData.push(req.body);
+  res.json();
+});
+
+logs.get("/:arrayIndex", (req, res) => {
+  const { arrayIndex } = req.params;
+  if (logsData[arrayIndex]) {
+    res.json(logsData[arrayIndex]);
+  } else {
+    res.status(404).json({ error: "Not found!" });
+  }
+});
+
+logs.delete("/:indexArray", (req, res) => {
+  const deletedLog = logsData.splice(req.params.indexArray, 1);
+  res.status(200).json(deletedLog);
+});
 
 module.exports = logs;
