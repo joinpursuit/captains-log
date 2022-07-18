3162513ae23aacec775f25ff0f1dd12cb7e7edf5
Edwin Codrington <edwincodrington@gmail.com>
2022-07-17 22:27:47 -0400

    logs controller implemented for GET, but not POST

--- controllers/logs.controller.js
+++ controllers/logs.controller.js
@@ -3,10 +3,21 @@ const logs = express.Router();
 let logsData = require("../models/log.js");
 
 logs.get("/", (request, response) => {
-    if(request.query){
-        
-    }
 	response.json(logsData);
+});
+logs.get("/:id", (request, response) => {
+	const { id } = request.params;
+	if (logsData[id]) {
+		response.json(logsData[id]);
+	} else {
+		//redirect to 404 from app.js
+		response.redirect("*");
+	}
+});
+logs.post("/", (request, response) => {
+	console.log("POST REQUESTED");
+	//logsData.push(request.query);
+	response.status(303).send("help, and lots of it");
 });
 
 module.exports = logs;
