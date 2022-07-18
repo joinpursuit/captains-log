f6f83938cfc9a5349fdaacef179ccd113747f9f1
CandaceNia <98069665+CandaceNia@users.noreply.github.com>
2022-07-18 13:27:02 -0400

    Bonus done.
    Updated bonus file and app.js tests. Still passing.

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const app = require("./app.js");
 
-
 require("dotenv").config();
 
 const PORT = process.env.PORT;
 
-
 app.listen(PORT, () => {
-	console.log(`It is listening on ${PORT} go Ahead !!!`);
+  console.log(`It is listening on ${PORT} go Ahead !!!`);
 });
