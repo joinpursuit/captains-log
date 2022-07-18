983b916da245837d85af9923d4484339bada0f4e
EmaleeSoto <emaleesoto@gmail.com>
2022-07-18 12:05:41 -0400

    Finished all tests, deletes item

--- controllers/logs.controller.js
+++ controllers/logs.controller.js
@@ -2,6 +2,13 @@ const express = require("express");
 const logs = express.Router();
 const logsArray = require("../models/log.js");
 
+logs.use("/:id", (req, res, next) => {
+  if (!logsArray[req.params.id]) {
+    res.redirect(404).send("Not Found");
+  }
+  next();
+});
+
 logs.post("/", (req, res) => {
   logsArray.push(req.body);
   res.json(logsArray[logsArray.length - 1]);
@@ -13,10 +20,19 @@ logs.get("/", (req, res) => {
 
 logs.get("/:id", (req, res) => {
   const id = parseInt(req.params.id);
-  if (id < 0 || id >= logsArray.length) {
-    res.redirect(404).json({ error: "Not found" });
-  }
   res.json(logsArray[id]);
+});
+
+logs.put("/:id", (req, res) => {
+  const { id } = req.params;
+  logs[id] = req.body;
+  res.send(logs[id]);
+});
+
+logs.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  const deletedItem = logsArray.splice(id, 1);
+  res.send(deletedItem);
 });
 
 module.exports = logs;
