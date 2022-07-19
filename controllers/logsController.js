531fec9950bab61203d9d0ad8a2183d6d192677a
Andres Varon <anvaron@gmail.com>
2022-07-19 08:55:32 -0500

    DELETE         ✓ deletes at the index in the logs array - tested

--- controllers/logsController.js
+++ controllers/logsController.js
@@ -1,38 +1,38 @@
 // DEPENDENCIES
 const express = require("express");
+const logsData = require("../models/log.js");
 
 // CONFIG
 const logs = express.Router();
-const logsData = require("../models/log.js");
+
 
 // ROUTES
 
 // GET => index
 logs.get("/", (req, res) => {
   //
-  res.send(logsData);
+  res.json(logsData);
 });
 
 // GET => Show
 logs.get("/:id", (req, res) => {
   const { id } = req.params;
   //
-  (logsData[id]) ? res.json(logsData[id]) : res.status(404).redirect('/')
+  (logsData[id]) ? res.json(logsData[id]) : res.status(404).redirect('/error')
 });
 
-// // POST => Create
+// POST => Create
 logs.post("/", (req, res) => {
-  console.log(req)
   logsData.push(req.body);
-  res.json(logsData[logsData.length - 1]);
+  res.send(logsData[logsData.length - 1]);
 });
 
-// // DELETE => Destroy
+
+// DELETE => Destroy
 logs.delete("/:indexArray", (req, res) => {
   const deletedLog = logsData.splice(req.params.indexArray, 1);
   res.status(200).json(deletedLog);
 });
-
 
 // EXPORT
 module.exports = logs;
