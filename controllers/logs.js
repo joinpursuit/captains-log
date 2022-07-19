0de1eceba2a1c933cccfd9df698eac6806a59ab8
T.K <nique@niques-air.myfiosgateway.com>
2022-07-18 20:47:45 -0400

    updating code

--- controllers/logs.js
+++ controllers/logs.js
@@ -3,15 +3,18 @@ const logs = express.Router();
 
 const logsData = require("../models/log.js");
 
+//INDEX
 logs.get("/", (req, res) => {
   res.json(logsData);
 });
 
+//CREATE
 logs.post("/", (req, res) => {
   logsData.push(req.body);
   res.json();
 });
 
+//SHOW
 logs.get("/:arrayIndex", (req, res) => {
   const { arrayIndex } = req.params;
   if (logsData[arrayIndex]) {
@@ -21,9 +24,17 @@ logs.get("/:arrayIndex", (req, res) => {
   }
 });
 
-logs.delete("/:indexArray", (req, res) => {
-  const deletedLog = logsData.splice(req.params.indexArray, 1);
+//DESTROY
+logs.delete("/:arrayIndex", (req, res) => {
+  const deletedLog = logsData.splice(req.params.arrayIndex, 1);
   res.status(200).json(deletedLog);
+});
+
+//UPDATE
+logs.put("/:arrayIndex", (req, res) => {
+  const { arrayIndex } = req.params;
+  logsData[arrayIndex] = req.body;
+  res.status(200).json(logsData[arrayIndex]);
 });
 
 module.exports = logs;
