78f7bf41f3d2dada0d30d37d2344c425c7f8e7ff
ziqian pan <ziqianpan1998@gmail.com>
2022-07-18 20:41:52 -0400

    finished lab added delete and update functionality

--- controllers/logsController.js
+++ controllers/logsController.js
@@ -25,10 +25,17 @@ logs.post("/", (req, res) => {
 });
 
 
-// logs.delete("/", (req, res) => {
-//     logData.pop(req.body);
-//     res.json(logData[logData.length - 1]);
-//     // res.send("deleted....GONE")
-// });
+logs.delete("/:arrayIndex", (req, res) => {
+    const {arrayIndex} = req.params;
+    const deleteLog = logData.splice(arrayIndex, 1);
+    res.status(200).json(deleteLog)
+});
+
+logs.put("/:arrayIndex", (req,res) => {
+    const {arrayIndex} = req.params
+    logData[arrayIndex] = req.body
+    res.status(200).json(logData[arrayIndex])
+});
+
 
 module.exports = logs
