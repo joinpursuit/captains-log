d7b30e98312cbf39cfc4c3b592b958faaab5ea85
jede <jedebrahma@pursuit.org>
2022-07-18 22:14:12 -0400

    delete added

--- controllers/logsController.js
+++ controllers/logsController.js
@@ -20,5 +20,16 @@ logs.post("/", (req, res) => {
     res.json(logsData[logsData.length - 1]);
 });
 
+logs.delete("/:arrayIndex", (req, res) => {
+    const { arrayIndex } = req.params;
+    const deleteLog = logsData.splice(arrayIndex, 1);
+    res.status(200).json(deleteLog)
+});
+
+logs.put("/:arrayIndex", (req,res) => {
+    const {arrayIndex} = req.params
+    logsData[arrayIndex] = req.body
+    res.status(200).json(logsData[arrayIndex])
+});
 
 module.exports = logs
