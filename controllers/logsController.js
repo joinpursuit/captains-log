a1496c3bb7695ea2ca52d8497c3b2b20f03e3a2d
Zane <zaneahmed4@gmail.com>
2022-07-18 20:34:47 -0400

    lab complete

--- controllers/logsController.js
+++ controllers/logsController.js
@@ -15,8 +15,29 @@ logs.get("/", (req, res) => {
 
 logs.get("/:arrayIndex", (req, res) => {
     const { arrayIndex } = req.params;
-    res.json(logsData[arrayIndex]);
+    if(logsData[arrayIndex]){
+      res.json(logsData[arrayIndex]);
+    } else {
+      res.redirect("/")
+    }
   });
+
+  logs.delete("/:arrayIndex", (req, res)=> {
+    const {arrayIndex} = req.params
+    const deletedLogs = logsData.splice(arrayIndex, 1)
+    res.status(200).json(deletedLogs)
+});
+
+logs.put("/:arrayIndex", (req,res) => {
+    const {arrayIndex} = req.params
+    if(logsData[arrayIndex]){
+      logsData[arrayIndex] = req.body
+      res.status(200).json(logsData[arrayIndex])
+    } else {
+      res.redirect("/")
+    }
+});
+
 
 
 module.exports = logs
