c9c82f5d5893674ff55078bcfbbf173760f3d712
Jorge Carrera <JCarrera994@gmail.com>
2022-07-18 20:55:05 -0400

    adds paths for DELETE and PUT functionality

--- controllers/logs.controller.js
+++ controllers/logs.controller.js
@@ -8,6 +8,7 @@ logs.get("/", (req, res) => {
 
 logs.get("/:arrayIndex", (req, res) => {
   const { arrayIndex } = req.params;
+  
   if (logsData[arrayIndex]) {
     res.json(logsData[arrayIndex]);
   } else {
@@ -18,6 +19,23 @@ logs.get("/:arrayIndex", (req, res) => {
 logs.post("/", (req, res) => {
   logsData.push(req.body);
   res.json(logsData[logsData.length - 1]);
+});
+
+logs.delete("/:arrayIndex", (req, res) => {
+  const { arrayIndex } = req.params;
+
+  if (logsData[arrayIndex]) {
+    deletedLog = logsData.splice(arrayIndex, 1);
+    res.status(303).json(deletedLog);
+  } else {
+    res.status(404).redirect("/Error");
+  }
+});
+
+logs.put("/:arrayIndex", (req, res) => {
+  const { arrayIndex } = req.params;
+  logsData[arrayIndex] = req.body;
+  res.status(200).json(logsData[arrayIndex]);
 });
 
 module.exports = logs;
