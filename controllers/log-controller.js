33c64e014a77106672c39c7f8a5a699ebcdb37ce
H.C.Guerrero <clydeguerrero1@gmail.com>
2022-07-19 10:19:06 -0300

    submission commit, redirect test still failing.

--- controllers/log-controller.js
+++ controllers/log-controller.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const captainLogs = require('../models/log');
+router.use('/*', (request, response, next) => {
+  if (request.params.arrayIndex && !captainLogs[request.params.arrayIndex]) {
+    response.status(404).send('Not Found');
+  } else {
+    next();
+  }
+});
 
 router.get('/', (request, response) => {
   response.send(captainLogs);
 });
-/*router.get('/', (request, response) => {
-  response.json(captainLogs);
-}); */
+
 router.post('/', (request, response) => {
   captainLogs.push(request.body);
   response.json(captainLogs[captainLogs.length - 1]);
@@ -16,7 +21,7 @@ router.get('/:arrayIndex', (request, response) => {
   if (captainLogs[request.params.arrayIndex]) {
     response.json(captainLogs[request.params.arrayIndex]);
   } else {
-    response.status(403).send('Not a valid Index!');
+    response.status(404).send('Not a valid Index!');
   }
 });
 router.delete('/:arrayIndex', (request, response) => {
