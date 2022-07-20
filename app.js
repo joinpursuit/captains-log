ae35b2caff9e4688a6115a65e1011b1303b840ef
named.josie <named.josie@gmail.com>
2022-07-20 07:13:38 -0400

    updated files to connect with frontend

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express');
-
 const app = express();
-
 const logsController = require('./controllers/logs.controller.js');
+const cors = require('cors')
+//used cors middlieware  to attach headers to the the http response
 
+
+app.use(cors())
 app.use(express.json());
-
 app.get('/', (req, res) => {
   res.send("✨Welcome  To The Captain's Log✨");
 });
