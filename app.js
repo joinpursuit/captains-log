5f884311ab0187234ec9025e7066ef1f1d366ccc
Kalilah Clarke <kalilah.clarke@gmail.com>
2022-07-23 17:54:44 -0400

    Next() add

--- app.js
+++ app.js
@@ -5,17 +5,18 @@ const cors = require('cors')
 app.use(express.json())
 app.use(cors())
 
-const logController = require("./controllers/logsController.js")
+const logsController = require("./controllers/logsController.js")
 
-app.use((req,res)=>{
+app.use((req,res, next)=>{
   console.log('A request is being made')
+  next()
 })
 
 app.get('/',(req,res)=>{
   res.send("Welcome to the captain's log")
 })
 
-app.use('/logs', logController);
+app.use('/logs', logsController);
 
 app.get('*',(req,res)=>{
   res.status(404).json({
