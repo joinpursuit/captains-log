659a27feef011f992b851caf96b701860adbd385
Cesar Ortiz <ceazthemoment@aol.com>
2022-08-08 06:48:57 -0400

    fixed

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
 const logController = require("./controllers/log.controller");
+const cors = require("cors");
 
 app.use(express.json());
+
+app.use(cors());
+
 app.use("/logs", logController);
 
 app.get("/", (req, res) => {
